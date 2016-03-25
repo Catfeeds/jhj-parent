ee04d6a230f7b4cf9217147a202d7852f8794754
leon <leon@57d5540f-7636-4a73-8af3-7dc9f519f443>
2016-03-25 06:08:38 +0000

    git-svn-id: svn://123.57.173.36/jhj-parent2.0/Branches/jhj-parent2.1@268 57d5540f-7636-4a73-8af3-7dc9f519f443

--- update-jhj-app.sh
+++ update-jhj-app.sh
@@ -4,6 +4,6 @@ mvn clean package
 /data/tomcat/bin/shutdown.sh
 sleep 1
 rm -rf /data/tomcat/webapps/jhj-app
-\cp -rf /data/src/jhj-parent2.1/jhj-app2.1/target/jhj-app.war /data/tomcat/webapps/
+\cp -rf /data/src/jhj-parent2.1/jhj-app/target/jhj-app.war /data/tomcat/webapps/
 /data/tomcat/bin/startup.sh
 
--- update-jhj-oa.sh
+++ update-jhj-oa.sh
@@ -4,5 +4,5 @@ mvn clean package
 /data/tomcat/bin/shutdown.sh
 sleep 1
 rm -rf /data/tomcat/webapps/jhj-oa
-\cp -rf /data/src/jhj-parent2.1/jhj-oa2.1/target/jhj-oa.war /data/tomcat/webapps/
+\cp -rf /data/src/jhj-parent2.1/jhj-oa/target/jhj-oa.war /data/tomcat/webapps/
 /data/tomcat/bin/startup.sh
