64e6a48ad82842597258d63ab650175e77f267fb
leon <leon@57d5540f-7636-4a73-8af3-7dc9f519f443>
2016-03-30 04:17:20 +0000

    git-svn-id: svn://123.57.173.36/jhj-parent2.0/Branches/jhj-parent2.1@307 57d5540f-7636-4a73-8af3-7dc9f519f443

--- jhj-oa/src/main/java/com/jhj/action/order/NewOrderDisController.java
+++ jhj-oa/src/main/java/com/jhj/action/order/NewOrderDisController.java
@@ -311,7 +311,8 @@ public class NewOrderDisController extends BaseController {
 		
 		
 		// 发推送消息
-		orderPayService.orderPaySuccessToDoForAm(order);
+//		orderPayService.orderPaySuccessToDoForAm(order);
+		orderSevice.userOrderAmSuccessTodo(order.getOrderNo());
 		
 		return resultData;
 	}
