5912320c308194dc2cbefb27e62f772bd33c716b
leon <leon@57d5540f-7636-4a73-8af3-7dc9f519f443>
2016-04-06 03:26:39 +0000

    git-svn-id: svn://123.57.173.36/jhj-parent2.0/Branches/jhj-parent2.1@415 57d5540f-7636-4a73-8af3-7dc9f519f443

--- jhj-web-user/js/index.js
+++ jhj-web-user/js/index.js
@@ -4,48 +4,10 @@ $$("#indexPageContentDiv").find("a").on("click",function(k,v){
 	
 });
 
-$$(document).on('pageBeforeInit', function(e) {
-	var page = e.detail.page;
+function orderLink(url, serviceTypeId) {
 	
-	if (page.name === 'index') {
-		
-		$$(".mincircle").on("click",function(k,v){
-			
-			var content =  $$(this).find("p").text();
-			
-			//给一个默认的 跳转页
-			var url = "index.html";
-			
-			var firstServiceType = "";
-			
-			if(content == '金牌保洁'){
-				url = "order/fiveservice/jinpaibaojie.html?firstServiceType=23";
-				firstServiceType = 23;
-			}
-			
-			if(content == '厨娘烧饭'){
-				url = "order/fiveservice/cook.html?firstServiceType=24";
-				firstServiceType = 24;
-			}
-			
-			if(content == '贴心家事'){
-				url = "order/fiveservice/tiexinjiashi.html?firstServiceType=25";
-				firstServiceType = 25;
-			}
-			
-			if(content == '深度养护'){
-				url = "order/fiveservice/tiexinjiashi.html?firstServiceType=26";
-				firstServiceType = 26;
-			}
-			
-			if(content == '企业服务'){
-				url = "order/fiveservice/tiexinjiashi.html?firstServiceType=27";
-				firstServiceType = 27;
-			}
-			
-			localStorage.setItem("firstServiceType",firstServiceType);
-			
-			mainView.router.loadPage(url);			
-		});
-	}
-})
+	localStorage.setItem("firstServiceType",serviceTypeId);
+	url = url + "?firstServiceType="+serviceTypeId;
+	mainView.router.loadPage(url);
+
+}
