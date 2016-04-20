c1795320338b311d0fa66eb11dedf0848f28e570
hulinjiang <hulinjiang@57d5540f-7636-4a73-8af3-7dc9f519f443>
2016-04-20 07:56:13 +0000

    git-svn-id: svn://123.57.173.36/jhj-parent2.0/Branches/jhj-parent2.1@500 57d5540f-7636-4a73-8af3-7dc9f519f443

---
+++ jhj-web-user/js/order/fiveservice/qiyefuwu.js
@@ -0,0 +1,59 @@
+//列表显示
+myApp.template7Data['page:qiyefuwu-page'] = function() {
+	var result;
+	
+	var parentServiceTypeId = localStorage['firstServiceType'];
+	
+	$$.ajax({
+		type : "GET",
+		url : siteAPIPath + "newPartServiceType/second_service_type.json",
+		data:{
+			"service_type_id":parentServiceTypeId
+		},
+		dataType : "json",
+		cache : true,
+		async : false,
+		success : function(data) {
+			result = data;
+			
+		}
+	})
+
+	return result;
+}
+
+
+myApp.onPageInit('qiyefuwu-page', function(page) {
+
+	// 根服务类型Id
+	var parentServiceTypeId = localStorage['firstServiceType'];
+	
+	$$("#parentServiceTypeId").val(parentServiceTypeId);
+
+	$$("div[name='tiexinjiashidiv']").on("click",function() {
+
+		var userId = localStorage['user_id'];
+
+		if (userId == undefined || userId.length == 0) {
+
+			myApp.alert("您还没有登录");
+
+			mainView.router.loadPage("login.html");
+			return false;
+		}
+
+		var enable = $$(this).find("input[name='enable']").val();
+
+		if (enable == 0) {
+			return false;
+		}
+
+		var serviceTypeId = $$(this).find("input[name='serviceTypeId']").val();
+		mainView.router.loadPage("order/order-am-faqiyuyue.html?serviceType="+serviceTypeId
+				+ "&parentServiceTypeId=" + parentServiceTypeId);
+				
+	});
+});
+
+
+
