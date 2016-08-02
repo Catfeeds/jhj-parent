f358bf373f14cc91f1a066416de26a5138ebd785
zhangzhiqiang <zhangzhiqiang@57d5540f-7636-4a73-8af3-7dc9f519f443>
2016-08-02 01:39:48 +0000

    修改setItem中设置的key
    git-svn-id: svn://123.57.173.36/jhj-parent2.0/Branches/jhj-parent2.1@672 57d5540f-7636-4a73-8af3-7dc9f519f443

--- jhj-web-user/js/order/user-am-result.js
+++ jhj-web-user/js/order/user-am-result.js
@@ -4,7 +4,7 @@ myApp.onPageBeforeInit('order-am-result-page', function (page) {
 	//查看订单
 	$$("#order-am-result").on("click", function() {
 	
-		localStorage.setItem('u_order_no_param', orderNo);
+		localStorage.setItem('order_no', orderNo);
 		//调整订单跳转
 		mainView.router.loadPage("order/order-view-2.html?order_no="+orderNo);
 
