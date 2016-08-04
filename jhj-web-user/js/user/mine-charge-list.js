ffadbac1fe8de0ef312977e1e50d8a68877b637b
zhangzhiqiang <zhangzhiqiang@57d5540f-7636-4a73-8af3-7dc9f519f443>
2016-08-04 08:01:57 +0000

    git-svn-id: svn://123.57.173.36/jhj-parent2.0/Branches/jhj-parent2.1@689 57d5540f-7636-4a73-8af3-7dc9f519f443

--- jhj-web-user/js/user/mine-charge-list.js
+++ jhj-web-user/js/user/mine-charge-list.js
@@ -23,31 +23,33 @@ myApp.onPageInit('mine-charge-list', function (page) {
 		return;
 	}
      $$(".charge-way").on("click",function(){
-    	 var cardId = $$(this).prev().val();
+    	 var cardId = $$(this).prev().prev().prev().val();
     	 var cardPay = $$(this).next().val();
-    	 if(cardPay < 10000){
-        	 mainView.router.loadPage("user/charge/mine-charge-way.html?card_id="+cardId+"&card_pay="+cardPay);
+    	 var sendMoney = $$(this).next().next().val();
+    	 if(cardPay <= 5000){
+        	 mainView.router.loadPage("user/charge/mine-charge-way.html?card_id="+cardId+"&card_pay="+cardPay
+        			 +"&send_money="+sendMoney);
     	 }
  	});
-     $$(".feed_back_money").on("click",function(){
-    	 mainView.router.loadPage("user/charge/mine-charge-fanquan.html");
-     });
+//     $$(".feed_back_money").on("click",function(){
+//    	 mainView.router.loadPage("user/charge/mine-charge-fanquan.html");
+//     });
 });
 
-function chargeLargeValue(obj) {
-	var amId = localStorage.getItem('am_id');
-	var amMobile = localStorage.getItem('am_mobile');
-	
-	if (localStorage.getItem('am_id') == null || localStorage.getItem('am_mobile') == null) {
-		
-		myApp.confirm('您还没有添加地址，点击确定前往添加地址立刻获得家庭助理，点击返回留在此页', "", function () {
-			mainView.router.loadPage("user/mine-add-addr.html?addr_id=0&return_url=user/charge/mine-charge-list.html");
-	    });
-	} else {
-	
-		myApp.confirm('土豪,大额充值可联系您的助理为您上门办理。确定拨打助理电话:'+amMobile, "", function () {
-			$$("#cal_am_mobile_real").attr("href", "tel:"+amMobile);
-			$$("#cal_am_mobile_real").click();
-	    });
-	}
-}
+//function chargeLargeValue(obj) {
+//	var amId = localStorage.getItem('am_id');
+//	var amMobile = localStorage.getItem('am_mobile');
+//	
+//	if (localStorage.getItem('am_id') == null || localStorage.getItem('am_mobile') == null) {
+//		
+//		myApp.confirm('您还没有添加地址，点击确定前往添加地址立刻获得家庭助理，点击返回留在此页', "", function () {
+//			mainView.router.loadPage("user/mine-add-addr.html?addr_id=0&return_url=user/charge/mine-charge-list.html");
+//	    });
+//	} else {
+//	
+//		myApp.confirm('土豪,大额充值可联系您的助理为您上门办理。确定拨打助理电话:'+amMobile, "", function () {
+//			$$("#cal_am_mobile_real").attr("href", "tel:"+amMobile);
+//			$$("#cal_am_mobile_real").click();
+//	    });
+//	}
+//}
