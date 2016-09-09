cdca80cec6d1a4edb4085de4e4fdfe7de7efe4be
zhangzhiqiang <zhangzhiqiang@57d5540f-7636-4a73-8af3-7dc9f519f443>
2016-09-09 09:04:22 +0000

    git-svn-id: svn://123.57.173.36/jhj-parent2.0/Branches/jhj-parent2.1@843 57d5540f-7636-4a73-8af3-7dc9f519f443

---
+++ jhj-dao/src/main/java/com/jhj/po/model/dict/CouponsType.java
@@ -0,0 +1,25 @@
+package com.jhj.po.model.dict;
+
+public class CouponsType {
+    private Integer couponsTypeId;
+
+    private String couponsTypeDesc;
+
+	public Integer getCouponsTypeId() {
+		return couponsTypeId;
+	}
+
+	public void setCouponsTypeId(Integer couponsTypeId) {
+		this.couponsTypeId = couponsTypeId;
+	}
+
+	public String getCouponsTypeDesc() {
+		return couponsTypeDesc;
+	}
+
+	public void setCouponsTypeDesc(String couponsTypeDesc) {
+		this.couponsTypeDesc = couponsTypeDesc;
+	}
+    
+
+}
