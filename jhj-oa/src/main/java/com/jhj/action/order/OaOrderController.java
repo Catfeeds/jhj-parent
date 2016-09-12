4fff2ed7c29a2289c5154cd5e407729fbfa7b86f
zhangzhiqiang <zhangzhiqiang@57d5540f-7636-4a73-8af3-7dc9f519f443>
2016-09-12 09:49:38 +0000

    删除order-hour-list 中的分页 ，添加分页会影响到查询的结果
    git-svn-id: svn://123.57.173.36/jhj-parent2.0/Branches/jhj-parent2.1@874 57d5540f-7636-4a73-8af3-7dc9f519f443

--- jhj-oa/src/main/java/com/jhj/action/order/OaOrderController.java
+++ jhj-oa/src/main/java/com/jhj/action/order/OaOrderController.java
@@ -158,7 +158,6 @@ public class OaOrderController extends BaseController {
 		int pageNo = ServletRequestUtils.getIntParameter(request, ConstantOa.PAGE_NO_NAME, ConstantOa.DEFAULT_PAGE_NO);
 		int pageSize = ServletRequestUtils.getIntParameter(request, ConstantOa.PAGE_SIZE_NAME, ConstantOa.DEFAULT_PAGE_SIZE);
 		// 分页
-		PageHelper.startPage(pageNo, pageSize);
 		
 		//查询条件的组合，需要做一些逻辑判断
 		//1. 如果为运营人员，则可以看所有的门店和所有状态
