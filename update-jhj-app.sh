5be08fb2fb7b255d351e587323073cf81dd0f9d2
lnczx <lnczx0915@gmail.com>
2016-09-24 16:44:48 +0800

    -- 自动更新脚本换成git

--- update-jhj-app.sh
+++ update-jhj-app.sh
@@ -1,9 +1,9 @@
-cd /data/src/jhj-parent2.1/
-svn up
+cd /data/src/jhj-parent/
+git pull
 mvn clean package
 /data/tomcat/bin/shutdown.sh
 sleep 1
 rm -rf /data/tomcat/webapps/jhj-app
-\cp -rf /data/src/jhj-parent2.1/jhj-app/target/jhj-app.war /data/tomcat/webapps/
+\cp -rf /data/src/jhj-parent/jhj-app/target/jhj-app.war /data/tomcat/webapps/
 /data/tomcat/bin/startup.sh
 
--- update-jhj-oa.sh
+++ update-jhj-oa.sh
@@ -1,8 +1,8 @@
-cd /data/src/jhj-parent2.1/
-svn up
+cd /data/src/jhj-parent/
+git pull
 mvn clean package
 /data/tomcat/bin/shutdown.sh
 sleep 1
 rm -rf /data/tomcat/webapps/jhj-oa
-\cp -rf /data/src/jhj-parent2.1/jhj-oa/target/jhj-oa.war /data/tomcat/webapps/
+\cp -rf /data/src/jhj-parent/jhj-oa/target/jhj-oa.war /data/tomcat/webapps/
 /data/tomcat/bin/startup.sh
