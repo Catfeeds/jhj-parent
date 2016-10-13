c9b2b805cd699144f7ae1a4a474f44844c35e159
lnczx <lnczx0915@gmail.com>
2016-10-13 12:12:37 +0800

    -- 更新编译微信版脚本

--- update-jhj-u.sh
+++ update-jhj-u.sh
@@ -1,17 +1,15 @@
-cd /data/src/u/
-svn revert index.html
-svn revert index1.html
-svn up
+cd /data/src/jhj-parent/jhj-web-user
+git reset --hard HEAD
+git pull
+cd /data/src/jhj-parent/jhj-web-user
+
 npm install grunt grunt-cli --save-dev
 npm install grunt-legacy-util grunt-legacy-log  grunt-contrib-cssmin grunt-contrib-connect grunt-contrib-concat grunt-contrib-less grunt-contrib-watch grunt-contrib-uglify load-grunt-tasks grunt-css asset-cache-control --save-dev
 npm install
-rm -rf /data/src/u/html
+rm -rf /data/src/jhj-parent/jhj-web-user/html
 
 grunt
 \cp  index1.html index.html
-rm -rf /data/src/u/index1.html
+rm -rf /data/src/jhj-parent/jhj-web-user/index1.html
 \cp -rf * /data/tomcat/webapps/u 
-
-
-
 
