84a0bad7ed12acbb98cc2b5f4ed9fa5db92dfb99
lnczx <lnczx0915@gmail.com>
2016-10-21 16:36:00 +0800

    -- npm 依赖库不需要每次都安装了

--- jhj-web-user/update-jhj-u.sh
+++ jhj-web-user/update-jhj-u.sh
@@ -3,9 +3,9 @@ git reset --hard HEAD
 git pull
 cd /data/src/jhj-parent/jhj-web-user
 
-npm install grunt grunt-cli --save-dev
-npm install grunt-legacy-util grunt-legacy-log  grunt-contrib-cssmin grunt-contrib-connect grunt-contrib-concat grunt-contrib-less grunt-contrib-watch grunt-contrib-uglify load-grunt-tasks grunt-css asset-cache-control --save-dev
-npm install
+#npm install grunt grunt-cli --save-dev
+#npm install grunt-legacy-util grunt-legacy-log  grunt-contrib-cssmin grunt-contrib-connect grunt-contrib-concat grunt-contrib-less grunt-contrib-watch grunt-contrib-uglify load-grunt-tasks grunt-css asset-cache-control --save-dev
+#npm install
 rm -rf /data/src/jhj-parent/jhj-web-user/html
 
 grunt
