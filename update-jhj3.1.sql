476f49841292d4c25dfba97b5b5fa75f7163e405
zhangzhiqiang <zha_zq@163.com>
2016-11-02 15:31:18 +0800

    ---添加sql

--- update-jhj3.1.sql
+++ update-jhj3.1.sql
@@ -3,3 +3,5 @@ ALTER TABLE `org_staffs` ADD `staff_code` VARCHAR(3) NOT NULL COMMENT '员工编
 ALTER TABLE `users` ADD `is_vip` TINYINT(1) NOT NULL DEFAULT '0' COMMENT '0 = 否（默认） 1= 是' ;
 
 ALTER TABLE `partner_service_type` ADD `mprice` DECIMAL(5,2) NOT NULL DEFAULT '0' COMMENT '会员价格' AFTER `price`;
+
+ALTER TABLE `order_cards` ADD `referee` VARCHAR(20) NOT NULL COMMENT '员工编号' ;
