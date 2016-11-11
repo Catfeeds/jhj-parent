c63f31224af2f41b01e54a451643d16b0d99caef
zhangzhiqiang <zha_zq@163.com>
2016-11-11 17:02:38 +0800

    ---修改充值页面

--- jhj-web-user/js/user/mine-charge-pay-success.js
+++ jhj-web-user/js/user/mine-charge-pay-success.js
@@ -1,34 +1,34 @@
 myApp.onPageBeforeInit('charge-pay-success-page', function(page) {
 	
-	var userId = localStorage.getItem("user_id");
-//	var paycardId=localStorage.getItem("pay_card_id");
-
-	var userInfoSuccess = function(data, textStatus, jqXHR) {
-	  	myApp.hideIndicator();
-		var result = JSON.parse(data.response);
-		if (result.status == "999") {
-			myApp.alert(data.msg);
-			return;
-		}
-		var userInfo = result.data; 
-		$$("#rest_money").text(userInfo.rest_money);
-	};
-	
-	//获取用户充值信息接口
-    $$.ajax({
-        type:"get",
-        url:siteAPIPath+"user/getUserRestMoneyInfo.json?user_id="+userId,
-        dataType:"json",
-        cache:false,
-        statusCode : {
-			200 : userInfoSuccess,
-			400 : ajaxError,
-			500 : ajaxError
-		}
-    });
-    
-    $$('#charge-pay-success-btn').click(function(){
-    	mainView.router.loadPage("user/mine.html");
-    });
-	
+//	var userId = localStorage.getItem("user_id");
+////	var paycardId=localStorage.getItem("pay_card_id");
+//
+//	var userInfoSuccess = function(data, textStatus, jqXHR) {
+//	  	myApp.hideIndicator();
+//		var result = JSON.parse(data.response);
+//		if (result.status == "999") {
+//			myApp.alert(data.msg);
+//			return;
+//		}
+//		var userInfo = result.data; 
+//		$$("#rest_money").text(userInfo.rest_money);
+//	};
+//	
+//	//获取用户充值信息接口
+//    $$.ajax({
+//        type:"get",
+//        url:siteAPIPath+"user/getUserRestMoneyInfo.json?user_id="+userId,
+//        dataType:"json",
+//        cache:false,
+//        statusCode : {
+//			200 : userInfoSuccess,
+//			400 : ajaxError,
+//			500 : ajaxError
+//		}
+//    });
+//    
+//    $$('#charge-pay-success-btn').click(function(){
+//    	mainView.router.loadPage("user/mine.html");
+//    });
+//	
 });
--- jhj-web-user/js/user/mine-charge-way.js
+++ jhj-web-user/js/user/mine-charge-way.js
@@ -1,50 +1,48 @@
 myApp.onPageBeforeInit('mine-charge-way', function(page) {
-	
+
 	var userId = localStorage.getItem("user_id");
 	var cardId = page.query.card_id;
-	var cardPay = page.query.card_pay;
+//	var cardPay = page.query.card_pay;
 	var orderPayType = 2;
 	localStorage.removeItem("pay_card_id");
 	localStorage.setItem("pay_card_id",cardId);
 	if (userId == undefined || userId == '' || userId == 0) {
 		return;
 	}
-	
+
 	$$("#mobile").text(localStorage.getItem("user_mobile"));
-	
-    $$("#card_pay_view").text(cardPay);
 
-    if (cardPay == undefined || cardPay == '' || cardPay == 0) {
-		var cardInfoSuccess = function(data, textStatus, jqXHR) {
-		  	myApp.hideIndicator();
-			var result = JSON.parse(data.response);
-			if (result.status == "999") {
-				myApp.alert(data.msg);
-				return;
-			}
-			var cardInfo = result.data; 
-			$$("#card_pay_view").text(cardInfo.card_value);
-			$$("#send-money").text(cardInfo.send_money);
-		};
-	
+//	var html = compiledTemplate(cardPay);
 
-		$$.ajax({
-	        type:"GET",
-	        url:siteAPIPath+"dict/get_card_detail.json?card_id="+cardId,
-	        dataType:"json",
-	        cache:false,
-	        statusCode : {
-				200 : cardInfoSuccess,
-				400 : ajaxError,
-				500 : ajaxError
-			}
-		}); 
-	}
-	
-	
+//	if (cardPay == undefined || cardPay == '' || cardPay == 0) {
+//		var cardInfoSuccess = function(data, textStatus, jqXHR) {
+//			myApp.hideIndicator();
+//			var result = JSON.parse(data.response);
+//			if (result.status == "999") {
+//				myApp.alert(data.msg);
+//				return;
+//			}
+//			var cardInfo = result.data;
+//			$$("#card_pay_view").text(cardInfo.card_value);
+//		};
+
+//		$$.ajax({
+//			type:"GET",
+//			url:siteAPIPath+"dict/get_card_detail.json?card_id="+cardId,
+//			dataType:"json",
+//			cache:false,
+//			statusCode : {
+//				200 : cardInfoSuccess,
+//				400 : ajaxError,
+//				500 : ajaxError
+//			}
+//		});
+//	}
+
+
 	var postCardBuySuccess =function(data, textStatus, jqXHR) {
 
-	 	var result = JSON.parse(data.response);
+		var result = JSON.parse(data.response);
 		if (result.status == "999") {
 			myApp.alert(result.msg);
 			return;
@@ -52,39 +50,52 @@ myApp.onPageBeforeInit('mine-charge-way', function(page) {
 		var orderId = result.data.id;
 		//如果为微信支付，则需要跳转到微信支付页面.
 		if (orderPayType == 2) {
-			 var wxPayUrl = localUrl + "/" + appName + "/wx-pay-pre.jsp";
-			 wxPayUrl +="?orderId="+orderId;
-			 wxPayUrl +="&userCouponId=0";
-			 wxPayUrl +="&orderType=4";
-			 wxPayUrl +="&payOrderType=1";
-			 location.href = wxPayUrl;
+			var wxPayUrl = localUrl + "/" + appName + "/wx-pay-pre.jsp";
+			wxPayUrl +="?orderId="+orderId;
+			wxPayUrl +="&userCouponId=0";
+			wxPayUrl +="&orderType=4";
+			wxPayUrl +="&payOrderType=1";
+			location.href = wxPayUrl;
 		}
-	};		
-	
-	//点击支付的处理
-	$$(".chongzhi-submit").click(function(){
+	};
 
-		
+	//点击支付的处理
+	$$("#chongzhi-submit").click(function(){
+
+
 		var postdata = {};
 		postdata.user_id = userId;
 		postdata.card_type = cardId;
 		postdata.pay_type = 2;
 		postdata.staff_code=$$("#staffCode").val();
-						
+
 		$$.ajax({
 			type: "post",
-			 url: siteAPIPath + "user/card_buy.json",
+			url: siteAPIPath + "user/card_buy.json",
 			data: postdata,
 			statusCode: {
-	         	200: postCardBuySuccess,
-	 	    	400: ajaxError,
-	 	    	500: ajaxError
-	 	    }
-		});		
-		 
-	});	
-    
-    
-    
-    
+				200: postCardBuySuccess,
+				400: ajaxError,
+				500: ajaxError
+			}
+		});
+
+	});
 });
+
+myApp.template7Data['page:mine-charge-way'] = function(){
+	var result
+	var cardId = sessionStorage.getItem("card_id");
+	$$.ajax({
+		type:"GET",
+		url:siteAPIPath+"dict/get_card_detail.json?card_id="+cardId,
+		dataType:"json",
+		cache:true,
+		async:false,
+		success :function(data) {
+			result=data.data;
+		}
+	});
+	sessionStorage.removeItem("card_id");
+	return result;
+}
