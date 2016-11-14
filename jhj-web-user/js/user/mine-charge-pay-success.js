87f31dcb954335e4b5c9994d1ef391897c60f750
zhangzhiqiang <zha_zq@163.com>
2016-11-14 13:14:46 +0800

    --修改页面

--- jhj-web-user/js/user/mine-charge-pay-success.js
+++ jhj-web-user/js/user/mine-charge-pay-success.js
@@ -1,31 +1,33 @@
-myApp.onPageBeforeInit('charge-pay-success-page', function(page) {
+myApp.onPageBeforeInit('mine-charge-pay-success', function(page) {
 	
-//	var userId = localStorage.getItem("user_id");
-////	var paycardId=localStorage.getItem("pay_card_id");
-//
-//	var userInfoSuccess = function(data, textStatus, jqXHR) {
-//	  	myApp.hideIndicator();
-//		var result = JSON.parse(data.response);
-//		if (result.status == "999") {
-//			myApp.alert(data.msg);
-//			return;
-//		}
-//		var userInfo = result.data; 
+	var userId = localStorage.getItem("user_id");
+//	var paycardId=localStorage.getItem("pay_card_id");
+	var userInfoSuccess = function(data, textStatus, jqXHR) {
+	  	myApp.hideIndicator();
+		var result = JSON.parse(data.response);
+		if (result.status == "999") {
+			myApp.alert(data.msg);
+			return;
+		}
+		var userInfo = result.data; 
 //		$$("#rest_money").text(userInfo.rest_money);
-//	};
-//	
-//	//获取用户充值信息接口
-//    $$.ajax({
-//        type:"get",
-//        url:siteAPIPath+"user/getUserRestMoneyInfo.json?user_id="+userId,
-//        dataType:"json",
-//        cache:false,
-//        statusCode : {
-//			200 : userInfoSuccess,
-//			400 : ajaxError,
-//			500 : ajaxError
-//		}
-//    });
+
+		localStorage.setItem("is_vip",userInfo.is_vip);
+		
+	};
+	
+	//获取用户充值信息接口
+    $$.ajax({
+        type:"get",
+        url:siteAPIPath+"user/getUserRestMoneyInfo.json?user_id="+userId,
+        dataType:"json",
+        cache:false,
+        statusCode : {
+			200 : userInfoSuccess,
+			400 : ajaxError,
+			500 : ajaxError
+		}
+    });
 //    
 //    $$('#charge-pay-success-btn').click(function(){
 //    	mainView.router.loadPage("user/mine.html");
