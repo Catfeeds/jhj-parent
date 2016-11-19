04cf67875d4deab5e2a60aeca3061d7cdb9538c5
lnczx <lnczx0915@gmail.com>
2016-11-19 19:49:13 +0800

    -- 用户账务明细

--- jhj-web-user/js/user/mine-rest-money-detail.js
+++ jhj-web-user/js/user/mine-rest-money-detail.js
@@ -1,102 +1,84 @@
 myApp.onPageInit('mine-rest-money-detail-page', function (page) {
 	
-	var postdata = {};
-	var page = 1;
 	var userId = localStorage['user_id'];
 	var loading = false;// 加载flag
-	var apiUrl = "user/pay_detail_list.json";
-	postdata.user_id = userId;
-	postdata.page = page;
+	var page = $$("#page").val();
 	
-	
-	var displayItem;
-	
-	
-	var orderListSuccess= function(data, textStatus, jqXHR) {
-		
+	var restDetailListSuccess = function(data, textStatus, jqXHR) {
+
 		var result = JSON.parse(data.response);
-		
-		displayItem = result.data;
-		
-		if(page >=2){
-			myList.appendItems(displayItem);
+		var datas = result.data;
+
+		var htmlTemplate = $$('#rest-detail-list-part').html();
+
+		var html = ''; // 当前订单
+
+		for (var i = 0; i < datas.length; i++) {
+			var item = datas[i];
+			var htmlPart = htmlTemplate;
+			
+			
+			var orderType = item.order_type;
+			var detailImg = "";
+			if (orderType == 0 || orderType == 1 || orderType == 2 || orderType == 3) {
+				detailImg = '<img src="img/laonian/jianhao.png" alt="" class="all-img7">';
+			} else {
+				detailImg = '<img src="img/laonian/jiahao.png" alt="" class="all-img7">';
+			}
+			htmlPart = htmlPart.replace(new RegExp('{detailImg}', "gm"), detailImg);
+			
+			htmlPart = htmlPart.replace(new RegExp('{orderTypeName}', "gm"), item.order_type_name);
+			htmlPart = htmlPart.replace(new RegExp('{orderPay}', "gm"), item.order_pay);
+			htmlPart = htmlPart.replace(new RegExp('{addTimeStr}', "gm"), item.add_time_str);
+			
+			html+= htmlPart;
 		}
-		
-		page = page+1;
+		// 当前订单
+		if (page == 1) {
+			$$("#rest-detail-list").html(html);
+		} else {
+			$$("#rest-detail-list").append(html);
+		}
+
 		loading = false;
 		
-		if(displayItem.length == 0){
-			loading = true;
-			$$("#pay-detail-list-more").text("已无更多记录(*^__^*)");
+		$$("#page").val(page);
+		if (datas.length >= 10) {
+			$$('#pay-detail-list-more').css("display", "block");
+		} else {
+			$$('#pay-detail-list-more').css("display", "none");
 		}
+	};
+	
+	
+	function loadRestDetailList(userId, page) {
+		console.log("page = " + page);
+		var postdata = {};
 		
-		return false;
-		
-	}	
-	
-	
-	postdata.user_id = userId;
-	postdata.page = page;
-	 $$.ajax({
-         type : "GET",
-         url  : siteAPIPath + apiUrl,
-         dataType: "json",
-         cache : true,
-         data : postdata,
-         async:false,	//设置为 同步。取到数据后。再 构造
- 		statusCode : {
- 			200 : orderListSuccess,
- 			400 : ajaxError,
- 			500 : ajaxError
- 		}
- 		
-	 });
-	
-
- var myList = myApp.virtualList('.list-block.virtual-list',{
-		
-		   items:  displayItem,
-		   height: 100,
-		template:  "<div class='pay-list-rv' >"
-						+ "<div class='pay-list-rv-left'>"
-						+  		"<img src='{{img_url}}' alt=''>"
-						+ "</div>"
-						+ "<div class='pay-list-rv-right'>"
-						+	"<div class='pay-list-rv-right1'>"
-						+		"<div class='pay-list-rv-right1-1'>{{order_type_name}}</div>"
-						+			"<div class='pay-list-rv-right1-2'>支付金额:{{order_money}}</div>"
-						+		"</div>"
-						+		"<div class='pay-list-rv-right2'>时间:{{add_time_str}}</div>"
-						+	"</div>"
-						+ "</div>"
-					+"</div>"
-	});
-	
-
-	 
-	 
-	// 注册'infinite'事件处理函数
-	$$('#pay-detail-list-more').on('click', function () {
-		if (loading) return;//如果正在加载，则退出
-		loading = true;// 设置flag
+		var apiUrl = "user/pay_detail_list.json";
 		postdata.user_id = userId;
-		
 		postdata.page = page;
-		
+
 		$$.ajax({
 			type : "GET",
-			url  : siteAPIPath + apiUrl,
+			url : siteAPIPath + apiUrl,
 			dataType : "json",
 			cache : true,
 			data : postdata,
-			timeout: 10000, //超时时间：10秒
-			async:false,
 			statusCode : {
-				200 : orderListSuccess,
+				200 : restDetailListSuccess,
 				400 : ajaxError,
 				500 : ajaxError
-			}
+			},
+
 		});
-	}); 
+	}	
+		
+	// 注册'infinite'事件处理函数
+	$$('#pay-detail-list-more').on('click', function() {
+		var cpage = ++page;
+		loadRestDetailList(userId, cpage);
+	});
 	
+	loadRestDetailList(userId, page);
 });
