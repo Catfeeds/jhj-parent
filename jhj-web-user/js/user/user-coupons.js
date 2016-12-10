330a0f667109d9c8c7961471534e9318cd6f6048
zhangzhiqiang <zha_zq@163.com>
2016-12-10 20:54:47 +0800

    ---添加满减

--- jhj-web-user/js/user/user-coupons.js
+++ jhj-web-user/js/user/user-coupons.js
@@ -7,6 +7,7 @@ myApp.onPageInit('mine-coupons-list', function (page) {
 
 	var backUrl = page.query.back_url;
 	var serviceTypeId = sessionStorage.getItem('service_type_id');
+	var orderMoney = sessionStorage.getItem('order_money');
 
 	//处理订单调整到当前页面选择优惠劵的
 	if(serviceTypeId!=null && serviceTypeId!='' && serviceTypeId!=undefined){
@@ -19,6 +20,7 @@ myApp.onPageInit('mine-coupons-list', function (page) {
 			var couponOrderType =  $$(this).find(".service_type").val();
 			var userCouponId = $$(this).find("#user_coupon_id").val();
 			var userCouponValue = $$(this).find("#user_coupon_value").val();
+			var maxVlaue = $$(this).find("#max_value").val();
 			var userCouponName = "￥" + userCouponValue;
 			
 			var fromDate = $$(this).find("#from_date").val();
@@ -35,6 +37,10 @@ myApp.onPageInit('mine-coupons-list', function (page) {
 				}
 			}
 			
+			
+			if(orderMoney>maxVlaue){
+				myApp.alert("当前优惠劵不适用!");
+			}
 //			}
 			
 			//判断有效期
