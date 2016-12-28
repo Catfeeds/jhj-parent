4a5d2133bc508f3d212c4bd58d604ce722ec75ea
lnczx <lnczx0915@gmail.com>
2016-12-28 18:27:18 +0800

    --整理用户消费明细脚本

--- cleanupUser.sql
+++ cleanupUser.sql
@@ -16,3 +16,18 @@ select distinct user_id from order_cards where add_time >= 1476892800 and order_
 
 //找出10.29之后充值的用户，之前有充值记录的
 select * from order_cards where order_status = 1 and add_time < 1476892800  and user_id in (select distinct user_id from order_cards where add_time >= 1476892800 and order_status = 1) 
+
+
+//清空用户余额，清空用户消费明细
+update users set rest_money = 0;
+truncate user_detail_pay;
+
+//更新消费明细，充值之前的余额清0
+update
+(SELECT * FROM `user_detail_pay` where order_type = 1 ) as a,
+user_detail_pay as b 
+set b.rest_money = 0
+where a.user_id = b.user_id a.add_time > b.add_time
+
+//余额为负数的，清0
+update users set rest_money = 0 where rest_money < 0
