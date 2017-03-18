472f5e6d067fdd8329ea914630e53d7452ec349c
zzq <zha_zq@163.com>
2017-03-18 10:11:47 +0800

    ---修改日历夸月

--- jhj-web-user/js/order/order-lib-cal.js
+++ jhj-web-user/js/order/order-lib-cal.js
@@ -99,26 +99,17 @@ myApp.onPageInit('order-lib-cal',function(page) {
         	 }else{
         		 for(var j=0;j<result.length;j++){
           			if(time[i]==result[j].service_hour){
-          				if(selectDate!='2017-02-27'){
           					
-          					if(result[j].is_full==0){
-          						if(time[i]==notSelectTime[0] || time[i]==notSelectTime[1] || time[i]==notSelectTime[2]){
-          							dateTime+="<li class='rilichange-no-time'><p>"+time[i]+"</p><p>约满</P></li>";
-          						}else{
-          							dateTime+="<li>"+time[i]+"</li>";
-          						}
-          					}
-          					if(result[j].is_full==1){
-          						dateTime+="<li class='rilichange-no-time'><p>"+time[i]+"</p><p>约满</p></li>";
-          					}
-          				}
-          				if(selectDate=='2017-02-27'){
-          					if(i<4){
-          						dateTime+="<li>"+time[i]+"</li>";
-          					}else{
-          						dateTime+="<li class='rilichange-no-time'><p>"+time[i]+"</p><p>约满</P></li>";
-          					}
-          		        }
+      					if(result[j].is_full==0){
+      						if(time[i]==notSelectTime[0] || time[i]==notSelectTime[1] || time[i]==notSelectTime[2]){
+      							dateTime+="<li class='rilichange-no-time'><p>"+time[i]+"</p><p>约满</P></li>";
+      						}else{
+      							dateTime+="<li>"+time[i]+"</li>";
+      						}
+      					}
+      					if(result[j].is_full==1){
+      						dateTime+="<li class='rilichange-no-time'><p>"+time[i]+"</p><p>约满</p></li>";
+      					}
           			}
           		}
         	 }
@@ -222,7 +213,7 @@ myApp.onPageInit('order-lib-cal',function(page) {
         $$("#calendar-day li:first-child").find("p").addClass("rilichange-day");
         isFull(getServiceDate(selectDay));
     }
-    getDay(date);
+//    getDay(date);
 
     //日期变化
     function getPreDay(selectDate,c){
