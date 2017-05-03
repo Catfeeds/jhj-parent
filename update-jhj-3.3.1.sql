0c52dade17287ce5415e6d1e67ad2f2e333dd790
lnczx <lnczx0915@gmail.com>
2017-05-03 17:05:27 +0800

    -- 跳转

--- update-jhj-3.3.1.sql
+++ update-jhj-3.3.1.sql
@@ -3,3 +3,11 @@ ALTER TABLE `partner_service_type` ADD `aprice` DECIMAL(5,2) NOT NULL DEFAULT '0
 
 
 ALTER TABLE `dict_service_addons` ADD `aprice` DECIMAL(5,2) NOT NULL DEFAULT '0' COMMENT '活动价' AFTER `staff_dis_price`, ADD `staff_aprice` DECIMAL(5,2) NOT NULL DEFAULT '0' COMMENT '活动价服务人员提成' AFTER `aprice`;
+
+
+
+INSERT INTO `cooperative_business` (`id`, `business_name`, `app_name`, `business_login_name`, `business_pass_word`, `enable`, `role_id`, `add_time`, `update_time`) VALUES
+(123, '微网站社区一', '微网站社区一', 'wwzsqy', '25d55ad283aa400af464c76d713c07ad', 1, 8, 1493798749, 1493798749),
+(124, '微网站社区二', '微网站社区二', 'wwzsqe', '25d55ad283aa400af464c76d713c07ad', 1, 8, 1493798796, 1493798796),
+(125, '微网站社区三', '微网站社区三', 'wwzsqs', '25d55ad283aa400af464c76d713c07ad', 1, 8, 1493798817, 1493798817),
+(126, '微网站社区四', '微网站社区四', 'wwzsqsi', '25d55ad283aa400af464c76d713c07ad', 1, 8, 1493798850, 1493798850);
