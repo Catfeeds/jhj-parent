710462c3bb2ec9005c0ca30d310a0d7a381b8b61
lnczx <lnczx0915@gmail.com>
2017-05-05 18:42:33 +0800

    -- host

--- jhj-app/src/main/java/com/jhj/action/OController.java
+++ jhj-app/src/main/java/com/jhj/action/OController.java
@@ -49,13 +49,13 @@ public class OController extends BaseController {
 			//检测来源ID是否存在
 			CooperativeBusiness record = cooperateBusinessService.selectByPrimaryKey(Long.valueOf(s));
 			if (record == null) return "";
-			String url = "http://localhost:8080/u/#!/active/20170504.html?order_op_from=" + s;
-//			String url = "";
-//			if (ConfigUtil.getInstance().getRb().getString("debug").equals("false")) {
-//				url = "http://www.jia-he-jia.com/u/#!/active/20170504.html?order_op_from=" + s;
-//			} else {
-//				url = "http://test.jia-he-jia.com/u/#!/active/20170504.html?order_op_from=" + s;
-//			}
+//			String url = "http://localhost:8080/u/#!/active/20170504.html?order_op_from=" + s;
+			String url = "";
+			if (ConfigUtil.getInstance().getRb().getString("debug").equals("false")) {
+				url = "http://www.jia-he-jia.com/u/#!/active/20170504.html?order_op_from=" + s;
+			} else {
+				url = "http://test.jia-he-jia.com/u/#!/active/20170504.html?order_op_from=" + s;
+			}
 			return "redirect:"+url;
 		}
 
