8b4de48cca829a5cc1075d51871665141c204bf8
zzq <zha_zq@163.com>
2017-05-16 15:14:34 +0800

    --

--- jhj-oa/src/main/webapp/WEB-INF/js/order/order-calendar.js
+++ jhj-oa/src/main/webapp/WEB-INF/js/order/order-calendar.js
@@ -325,8 +325,7 @@ $(function(){
 	
 	//获取选择的服务时间
 	$(document).on('click','#checkDate',function(){
-		var selectDate = getServiceDate(selectDay);
-		var st = selectDate+" "+dayTime+":00";
+		var st = date+" "+dayTime+":00";
 		if(dayTime!=""){
 			$("#serviceDate").val(st);
 			layer.close(layer.index);
