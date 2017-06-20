fbb428b3842f0ca34c8efa640a55e510b69e9f77
lnczx <lnczx0915@gmail.com>
2017-06-20 14:18:03 +0800

    -- sql

--- update/查询服务人员金额常用.sql
+++ update/查询服务人员金额常用.sql
@@ -12,3 +12,83 @@ select sum(order_pay) from order_prices where order_id in (select order_id from 
 订单加时总金额 = 45
 
 select sum(order_pay) from order_price_ext where  order_id in (select order_id from order_dispatchs where staff_id = 204 and dispatch_status = 1) and order_ext_type = 1
+
+
+//查询派工常用.
+select order_id as '订单号', staff_id as '员工ID', staff_name as '员工姓名', 
+ from_unixtime(service_date) as '服务开始时间', 
+ service_hours as '服务时长(小时)',
+ from_unixtime(service_date + 3600 * service_hours) as '服务结束时间',
+ from_unixtime(service_date + 3600 * service_hours + 3600 * 1.5) as '服务延后时间'
+
+ from order_dispatchs where 1=1 
+
+
+                        and staff_id in
+                         (  
+                                11
+                         , 
+                                24
+                         , 
+                                50
+                         , 
+                                53
+                         , 
+                                73
+                         , 
+                                88
+                         , 
+                                100
+                         , 
+                                121
+                         , 
+                                128
+                         , 
+                                129
+                         , 
+                                136
+                         , 
+                                141
+                         , 
+                                142
+                         , 
+                                145
+                         , 
+                                147
+                         , 
+                                152
+                         , 
+                                169
+                         , 
+                                178
+                         , 
+                                199
+                         , 
+                                207
+                         , 
+                                215
+                         , 
+                                220
+                         , 
+                                221
+                         , 
+                                222
+                         , 
+                                223
+                         , 
+                                226
+                         , 
+                                228
+                         ) 
+                 
+
+                 
+                        and dispatch_status = 1
+                 
+                 
+                             
+            and (
+                (service_date >= 1498005000  and service_date <= 1498021200 )
+                or
+                ((service_date+ service_hours*3600 + 1.5 * 3600) >= 1498005000 and (service_date+ service_hours*3600 + 1.5 * 3600) <= 1498021200)
+            )
